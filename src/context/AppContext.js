1cc98c66cb3d3900a272cb69816852c1a1175a14
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2021-03-25 11:15:28 -0700

    day 4
    the 'remainig', 'spent so far' now works through the state, now you can also add a new expense and delete expenses

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -4,12 +4,21 @@ const AppReducer = (state, action) =>{
     switch(action.type){
         case 'ADD_EXPENSE':
             return{
-                //copy the current state
+                //copy of the current state
                 ...state,
                 //create a new expenses object
                 expenses:[...state.expenses /*the new expenses contain de current expenses object*/,
                             action.payload/*plus the new expense*/]     
             }
+
+        case 'DELETE_EXPENSE':
+            return{
+                ...state,
+                expenses: state.expenses.filter(
+                    //return an array of the expenses without the expense with the id = payload
+                    (expense) => expense.id !== action.payload
+                ),
+            };
 
         default:
             return state;
@@ -18,16 +27,21 @@ const AppReducer = (state, action) =>{
 
 
 const initialState ={
-    budget:24000,
+    budget:3000,
     expenses:[
-        {id:12, name:'shopping', cost:32},
-        {id:12, name:'books', cost:32},
-        {id:12, name:'figures', cost:32},
-        {id:12, name:'session', cost:32},
-        {id:12, name:'week', cost:32},
-        {id:12, name:'games', cost:32}
+        {id:1, name:'Despensa', cost:300},
+        {id:2, name:'Ahorro', cost:500},
+        {id:3, name:'Bitso', cost:100},
+        {id:4, name:'Terapia', cost:400},
+        {id:5, name:'Celular', cost:25},
+        {id:6, name:'Mara TV faltan 1000', cost:125},
+        {id:7, name:'Credito HSBC', cost:600},
+        {id:8, name:'Genshin Impact', cost:125},
+        {id:9, name:'Mara Teclado faltan 1000', cost:125}
     ],
 };
+
+
 
 export const AppContext = createContext();
 
